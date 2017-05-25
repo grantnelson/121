6b7a97817a43a29a487245990ff9deb3373858bf
Daniel Sung <d2sung@ucsd.edu>
2017-05-24 23:45:55 -0700

    Draggable and remove bug done

--- public/js/UCSDTrek.js
+++ public/js/UCSDTrek.js
@@ -1,25 +1,27 @@
-var app = angular.module('UCSDTrek', []);
+var app = angular.module('UCSDTrek', ['ui.sortable']);
+
+//For data binding "mustache"
+app.config(function($interpolateProvider) {
+  $interpolateProvider.startSymbol('{[{');
+  $interpolateProvider.endSymbol('}]}');
+});
 
 
 
 app.controller('mainController', function($scope){
 
   $scope.numbers = [{}];
-
   $scope.courses = [{}];
 
   $scope.addClass = function(){
-
-      $scope.courses.push({});
+    $scope.courses.push({});
       setTimeout(function(){
         autocompleteInit();
       }, 1);
-
-    //$scope.number = $scope.number + 1;
    };
 
-  $scope.removeCourse = function(e){
-    angular.element(e.target).parent().parent().parent().parent().remove();
+  $scope.removeCourse = function(array, index){
+    array.splice(index, 1);
   };
 
 });
